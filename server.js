982c0cfef11a8c91709c9ca731dcfed9e9ba1aa0
MahmoodMakhloof <mahmoodmakhloof1998@gmail.com>
2022-03-14 17:01:36 +0200

    ADD & IMP server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express =require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+// mongodb connection
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/fota", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((error) => {
+    console.error("error! " + error);
+  });
+
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/",function(req,res){
+    res.send('Hello from server');
+})
+
+// import routers
+const carRoute = require('./routes/car_router');
+const userRoute = require('./routes/user_router');
+const firmwareRoute = require('./routes/firmware_router');
+const employeeRoute = require('./routes/employee_router');
+
+// use routes
+app.use("/api/v1",carRoute);
+app.use("/api/v1",userRoute);
+app.use("/api/v1",employeeRoute);
+app.use("/api/v1",firmwareRoute);
+
+// server listen
+app.listen(3000,()=>{
+    console.log('server start ....');
+})
