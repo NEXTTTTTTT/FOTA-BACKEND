580f52f714ada4d708414787bd66976363cb28b8
MahmoodMakhloof <mahmoodmakhloof1998@gmail.com>
2022-04-30 17:09:39 +0200

    update getCars function again

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -52,7 +52,8 @@ const userCtrl = {
   getCars: async (req, res) => {
     try {
       const id = req.params.id;
-      const myCars = await Cars.find({ admin:id, users: id,}).populate("firmware users admin", "-password");
+      console.log(id);
+      const myCars = await Cars.find({ admin:mongoose.ObjectId(id.toString), users: mongoose.ObjectId(id.toString),}).populate("firmware users admin", "-password");
       return res.status(200).json({"status":0 ,msg: "sucess", "my_cars" 
       : myCars, });
     } catch (err) {
