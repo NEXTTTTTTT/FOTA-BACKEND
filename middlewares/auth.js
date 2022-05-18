59e84dff55487c1c63d23527222578a6e329a9a4
MahmoodMakhloof <mahmoodmakhloof1998@gmail.com>
2022-05-19 01:56:13 +0200

    401 instead of 500

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,19 @@
-const Users = require("../model/user");
+
 const jwt = require('jsonwebtoken');
 
 const auth = async (req,res,next) =>{
     try {
         const token = req.header("Authorization")
 
-        if(!token)  return res.status(500).json({msg: "auth is required"})
+        if(!token)  return res.status(400).json({msg: "token is required"})
 
         const decoded = jwt.verify(token, process.env.ACCESSTOKENSECRET)
         if(!decoded)  return res.status(401).json({msg: "Invalid JWT"})
         
-        const user = await Users.findOne({_id: decoded.id})
-       
-        req.user = user;
-        
         next();
         
     } catch (err) {
-        return res.status(500).json({msg: err.message})
+        return res.status(401).json({msg: "Invalid JWT"})
     }
 }
 
