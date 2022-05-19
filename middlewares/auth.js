330448aa1d323724b41513b0c2a4a4c48f1d1be4
MahmoodMakhloof <mahmoodmakhloof1998@gmail.com>
2022-05-19 02:04:21 +0200

    undo

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,23 @@
-
+const Users = require("../model/user");
 const jwt = require('jsonwebtoken');
 
 const auth = async (req,res,next) =>{
     try {
         const token = req.header("Authorization")
 
-        if(!token)  return res.status(400).json({msg: "token is required"})
+        if(!token)  return res.status(500).json({msg: "token required"})
 
         const decoded = jwt.verify(token, process.env.ACCESSTOKENSECRET)
-        if(!decoded)  return res.status(401).json({msg: "Invalid JWT"})
+        if(!decoded)  return res.status(401).json({msg: "jwt expired"})
+        
+        const user = await Users.findOne({_id: decoded.id})
+       
+        req.user = user;
         
         next();
         
     } catch (err) {
-        return res.status(401).json({msg: "Invalid JWT"})
+        return res.status(401).json({msg: "jwt expired"})
     }
 }
 
